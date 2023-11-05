9edd29b8203f5a2383962b20bbf4bdfe7527c154
MagneticMartian <60756647+MagneticMartian@users.noreply.github.com>
2023-11-04 23:02:44 -0500

    Update 01_superscalar.C

--- Chapter03/01_superscalar.C
+++ Chapter03/01_superscalar.C
@@ -14,12 +14,10 @@ void BM_add(benchmark::State& state) {
     unsigned long* p1 = v1.data();
     unsigned long* p2 = v2.data();
     for (auto _ : state) {
-        unsigned long a1 = 0, a2 = 0;
+        unsigned long a1 = 0;
         for (size_t i = 0; i < N; ++i) {
-            a1 += p1[i] + p2[i];
+            benchmark::DoNotOptimize(a1 += p1[i] + p2[i]);
         }
-        benchmark::DoNotOptimize(a1);
-        benchmark::DoNotOptimize(a2);
         benchmark::ClobberMemory();
     }
     state.SetItemsProcessed(N*state.iterations());
@@ -37,12 +35,10 @@ void BM_multiply(benchmark::State& state) {
     unsigned long* p1 = v1.data();
     unsigned long* p2 = v2.data();
     for (auto _ : state) {
-        unsigned long a1 = 0, a2 = 0;
+        unsigned long a1 = 0;
         for (size_t i = 0; i < N; ++i) {
-            a2 += p1[i] * p2[i];
+            benchmark::DoNotOptimize(a1 += p1[i] * p2[i]);
         }
-        benchmark::DoNotOptimize(a1);
-        benchmark::DoNotOptimize(a2);
         benchmark::ClobberMemory();
     }
     state.SetItemsProcessed(N*state.iterations());
